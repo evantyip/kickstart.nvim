7001575f2def942fd5b041a6068585b61e510eb2
Evan Yip <evanyip@Evans-Air.attlocal.net>
2025-04-01 10:14:46 -0700

    Change settings for statuscol

--- lua/custom/plugins/statuscolumn.lua
+++ lua/custom/plugins/statuscolumn.lua
@@ -1,23 +1,19 @@
 return {
   "luukvbaal/statuscol.nvim", config = function()
-    vim.o.fillchars = [[eob: ,fold: ,foldopen:▼,foldsep: ,foldclose:⏵]]
-    vim.o.foldcolumn = '1'
+    vim.o.foldcolumn = "auto:1"
+    vim.o.fillchars = [[eob: ,fold: ,foldopen:▽,foldsep: ,foldclose:▷]]
     local builtin = require("statuscol.builtin")
     require("statuscol").setup({
-      -- configuration goes here, for example:
       relculright = true,
       segments = {
         { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
         {
-          sign = { namespace = { "diagnostic/signs" }, maxwidth = 2, auto = true },
-          click = "v:lua.ScSa"
+          text = { builtin.lnumfunc, " " },
+          condition = { true, builtin.not_empty },
+          click = "v:lua.ScLa",
         },
-        { text = { builtin.lnumfunc }, click = "v:lua.ScLa", },
-        {
-          sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
-          click = "v:lua.ScSa"
-        },
-      }
+        { text = { "%s" }, maxwidth = 2, colwidth = 1, click = "v:lua.ScSa" },
+      },
     })
   end,
 }
